994bae1f5b362616bdfe6adb33841dc01dc3097a
Basinger <fel_simon@mail.ru>
2023-05-08 16:21:44 +0300

    Added 3.2 data_about_human

---
+++ data_bout_hmn_3.2.py
@@ -0,0 +1,7 @@
+def info(first_n, second_n, birth, city, mail, phone):
+    return print(first_n, second_n, birth, city, mail, phone)
+
+
+info(first_n=input("Enter your name: "), second_n=input("Enter your second name: "),
+     birth=input("Enter your birthday: "), city=input("Enter your city: "),
+     mail=input("Enter your email: "), phone=input("Enter your pone: "))
