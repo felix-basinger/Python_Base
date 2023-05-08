c641e429da8aa98c5175982ff37542ed14e80986
Basinger <fel_simon@mail.ru>
2023-05-08 17:33:35 +0300

    Added changes in 3.3

--- E_3.3_sum_of_args.py
+++ E_3.3_sum_of_args.py
@@ -1,9 +1,16 @@
 def my_func(arg1, arg2, arg3):
-    a = arg1 + arg2
-    b = arg2 + arg3
-    c = arg1 + arg3
-    return print(f"Sum of the biggest nums = {max(a, b, c)}")
+    # a = arg1 + arg2
+    # b = arg2 + arg3
+    # c = arg1 + arg3
+    # return print(f"Sum of the biggest nums = {max(a, b, c)}")
+
+    my_list = [arg1, arg2, arg3]
+    my_list.sort()
+    my_list.remove(my_list[0])
+    result = sum(my_list)
+    return print(f"Result of sum {my_list[0]} + {my_list[1]} = {result}")
 
 
 my_func(int(input("Enter 1 num: ")), int(input("Enter 2 num: ")),
         int(input("Enter 3 num: ")))
+
