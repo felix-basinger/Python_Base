89a4ab98eaac1b9248cda7244cc2a2642baa2e1c
Basinger <fel_simon@mail.ru>
2023-05-13 00:52:37 +0300

    Added 4.2 num is more than

---
+++ E_4.2_num_is_more_than.py
@@ -0,0 +1,13 @@
+from random import randint
+
+my_list = [randint(-50, 50) for el in range(randint(10, 20))]
+print(my_list)
+for n in range(len(my_list)):
+    if n == len(my_list) - 1:
+        break
+    else:
+        new_list = [my_list[n + 1] for n in range(len(my_list) - 1) if my_list[n] < my_list[n + 1]]
+        print(new_list)
+        break
+
+
